dc828e247344abd8f6aebcc302ae569522b3bfff
Nithyananthan M <nithimech2023@gmail.com>
2017-09-26 14:48:28 +0000

    Initial Commit

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php		
-ini_set('display_errors', 1);
-	
+//ini_set('display_errors', 1);
+//error_reporting(E_ALL);
+
 require_once 'libraries/httpProxyClass.php';
 require_once 'libraries/cloudflareClass.php';
 
@@ -10,8 +11,11 @@ $httpProxyUA = 'proxyFactory';
 $requestLink = $_GET['u'];
 $requestPage = json_decode($httpProxy->performRequest($requestLink));
 
+if($requestPage->status->http_code == 200){
+		echo $requestPage->content;
+}
 // if page is protected by cloudflare
-if($requestPage->status->http_code == 503) {
+else if($requestPage->status->http_code == 503) {
 	// Make this the same user agent you use for other cURL requests in your app
 	cloudflare::useUserAgent($httpProxyUA);
 	
