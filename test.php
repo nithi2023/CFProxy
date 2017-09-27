692e46dd32bd7af06621dccd25d31fc07429bb60
Nithyananthan M <nithimech2023@gmail.com>
2017-09-27 14:11:54 +0000

    Changes

--- test.php
+++ test.php
@@ -5,7 +5,7 @@ require_once 'libraries/httpProxyClass.php';
 require_once 'libraries/cloudflareClass.php';
 
 $httpProxy   = new httpProxy();
-$httpProxyUA = 'proxyFactory';
+$httpProxyUA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36';
 
 $requestLink = $_GET['u'];
 $requestPage = json_decode($httpProxy->performRequest($requestLink));
